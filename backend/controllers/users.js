15aaffb5a11a6699a7841e1f476a33b9253d5155
k-ermolova <ermolowaksenia@yandex.ru>
2021-07-20 01:49:14 +0300

    fix: secret-key

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,12 +1,12 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const NotFoundError = require('../errors/not-found-err');
-const InternalServerError = require('../errors/internal-server-error');
-const BadRequestError = require('../errors/bad-request-err');
-const UnauthorizedError = require('../errors/unauthorized-err');
-const ConflictError = require('../errors/conflict-err');
+const NotFoundError = require("../errors/not-found-err");
+const InternalServerError = require("../errors/internal-server-error");
+const BadRequestError = require("../errors/bad-request-err");
+const UnauthorizedError = require("../errors/unauthorized-err");
+const ConflictError = require("../errors/conflict-err");
 
 const MONGO_DUPLICATE_ERROR_CODE = 11000;
 const SALT_ROUNDS = 10;
@@ -15,7 +15,7 @@ module.exports.getUsers = (req, res, next) => {
   User.find({}, { __v: 0 })
     .then((users) => res.send(users))
     .catch(() => {
-      throw new InternalServerError('На сервере произошла ошибка.');
+      throw new InternalServerError("На сервере произошла ошибка.");
     })
     .catch(next);
 };
@@ -26,14 +26,14 @@ module.exports.getCurrentUser = (req, res, next) => {
       if (user) {
         res.send(user);
       } else {
-        next(new NotFoundError('Пользователь по указанному _id не найден.'));
+        next(new NotFoundError("Пользователь по указанному _id не найден."));
       }
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Передан невалидный _id.');
+      if (err.name === "CastError") {
+        throw new BadRequestError("Передан невалидный _id.");
       } else {
-        throw new InternalServerError('На сервере произошла ошибка.');
+        throw new InternalServerError("На сервере произошла ошибка.");
       }
     })
     .catch(next);
@@ -45,36 +45,48 @@ module.exports.getUserById = (req, res, next) => {
       if (user) {
         res.send(user);
       } else {
-        next(new NotFoundError('Пользователь по указанному _id не найден.'));
+        next(new NotFoundError("Пользователь по указанному _id не найден."));
       }
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Передан невалидный _id.');
+      if (err.name === "CastError") {
+        throw new BadRequestError("Передан невалидный _id.");
       } else {
-        throw new InternalServerError('На сервере произошла ошибка.');
+        throw new InternalServerError("На сервере произошла ошибка.");
       }
     })
     .catch(next);
 };
 
 module.exports.createUser = (req, res, next) => {
-  const {
-    name, about, avatar, email, password,
-  } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
-  bcrypt.hash(password, SALT_ROUNDS)
-    .then((hash) => User.create({
-      name, about, avatar, email, password: hash,
-    }))
+  bcrypt
+    .hash(password, SALT_ROUNDS)
+    .then((hash) =>
+      User.create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      })
+    )
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные при создании пользователя.');
-      } else if (err.name === 'MongoError' && err.code === MONGO_DUPLICATE_ERROR_CODE) {
-        throw new ConflictError('Пользователь с указанным email уже существует.');
+      if (err.name === "ValidationError") {
+        throw new BadRequestError(
+          "Переданы некорректные данные при создании пользователя."
+        );
+      } else if (
+        err.name === "MongoError" &&
+        err.code === MONGO_DUPLICATE_ERROR_CODE
+      ) {
+        throw new ConflictError(
+          "Пользователь с указанным email уже существует."
+        );
       } else {
-        throw new InternalServerError('На сервере произошла ошибка.');
+        throw new InternalServerError("На сервере произошла ошибка.");
       }
     })
     .catch(next);
@@ -86,19 +98,21 @@ module.exports.updateProfile = (req, res, next) => {
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
-    { new: true, runValidators: true },
+    { new: true, runValidators: true }
   )
-    .orFail(new Error('NotFound'))
+    .orFail(new Error("NotFound"))
     .then((user) => {
       res.send(user);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные при обновлении профиля.');
+      if (err.name === "ValidationError") {
+        throw new BadRequestError(
+          "Переданы некорректные данные при обновлении профиля."
+        );
       } else if (err.name === err.message) {
-        throw new NotFoundError('Пользователь по указанному _id не найден.');
+        throw new NotFoundError("Пользователь по указанному _id не найден.");
       } else {
-        throw new InternalServerError('На сервере произошла ошибка.');
+        throw new InternalServerError("На сервере произошла ошибка.");
       }
     })
     .catch(next);
@@ -110,20 +124,22 @@ module.exports.updateAvatar = (req, res, next) => {
   User.findByIdAndUpdate(
     req.user._id,
     { avatar },
-    { new: true, runValidators: true },
+    { new: true, runValidators: true }
   )
-    .select('-__v')
-    .orFail(new Error('NotFound'))
+    .select("-__v")
+    .orFail(new Error("NotFound"))
     .then((user) => {
       res.send(user);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные при обновлении аватара.');
+      if (err.name === "ValidationError") {
+        throw new BadRequestError(
+          "Переданы некорректные данные при обновлении аватара."
+        );
       } else if (err.name === err.message) {
-        throw new NotFoundError('Пользователь по указанному _id не найден.');
+        throw new NotFoundError("Пользователь по указанному _id не найден.");
       } else {
-        throw new InternalServerError('На сервере произошла ошибка.');
+        throw new InternalServerError("На сервере произошла ошибка.");
       }
     })
     .catch(next);
@@ -134,7 +150,11 @@ module.exports.login = (req, res, next) => {
 
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'dev-secret', { expiresIn: '7d' });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+        { expiresIn: "7d" }
+      );
 
       res.send({ token });
     })
